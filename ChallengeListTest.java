5a4890034b9864f7d3aae5a63ccac62579f30144
unknown <matthieu.londeix.lp2@gmail.com>
2018-10-10 17:44:29 +0200

    asserttrue

--- ChallengeListTest.java
+++ ChallengeListTest.java
@@ -19,6 +19,7 @@ public class ChallengeListTest{
 
   public static void testCommenceFini(){
     // TODO @B
+    ChallengeList.commenceFini("AZERTYYTREAZ");
     assertTrue("Les deux lettres du début et de la fin sont identiques", true);
   }
 
